6d82a303b75bae3e3b88d8618ba0b33361acaca4
aguiro <aguimera2000@gmail.com>
2026-03-19 18:05:50 +0100

    feat: initial project setup

---
+++ src/test/java/com/aguiro/foodflow/FoodflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguiro.foodflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
